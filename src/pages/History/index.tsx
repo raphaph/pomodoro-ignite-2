aafca24fa728bf02d2d4cb0857a7f66493208957
Raphael Barros <rapha.wb@hotmail.com>
2023-02-22 21:26:29 -0300

    page history finalizada

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,7 +1,47 @@
+import { HistoryContainer, HistoryList, Status } from './styles'
+
 export function History() {
   return (
-    <div>
-      <h1>History</h1>
-    </div>
+    <HistoryContainer>
+      <h1>Meu histórico</h1>
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Hà 2 meses</td>
+              <td>
+                <Status stautsColor="green">Concluído</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Hà 2 meses</td>
+              <td>
+                <Status stautsColor="yellow">Em andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Hà 2 meses</td>
+              <td>
+                <Status stautsColor="red">Interrompido</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
   )
 }
